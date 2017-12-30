46a88d7800eb6b2a436bf69d1ca9320c83c4ff6a
gitbilla <augustine.abraham@outlook.com>
2017-12-30 20:03:14 +0530

    modiffied serverperformance count from 3 to 5

--- ServerPerformance.sh
+++ ServerPerformance.sh
@@ -52,4 +52,4 @@ echo -e "Disk Usage / (%) : $R "
 
 echo -e "\n\tProcess Status"
 echo -e "\nTop 3 Memory Eating Process\n "
-ps axo rss,comm,pid | awk '{ proc_list[$2]++; proc_list[$2 ","1] += $1; } END { for (proc in proc_list) { printf("%d\t%s\n", proc_list[proc "," 1],proc); }}' | sort -n | tail -n 3 | sort -rn | awk '{$1/=1024;printf "%.0fMB\t",$1}{print $2}'
+ps axo rss,comm,pid | awk '{ proc_list[$2]++; proc_list[$2 ","1] += $1; } END { for (proc in proc_list) { printf("%d\t%s\n", proc_list[proc "," 1],proc); }}' | sort -n | tail -n 5 | sort -rn | awk '{$1/=1024;printf "%.0fMB\t",$1}{print $2}'
