dd5915cdbf6e911ed5f1718a3c52c9c01eaffd83
gitbilla <augustine.abraham@outlook.com>
2018-01-02 12:46:40 +0530

    LVM_scripts

---
+++ LVM_scripts/mount.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# Script to mount the  filesystem in LVM
+# Author : Prashant Pilankar
+# Read the BigAdmin article that describes the usage of this script:
+# http://www.sun.com/bigadmin/content/submitted/lvm_admin.jsp
+echo "enter the vg and lv name and mount point for mounting the FS "
+echo "please ensure mount point directory exists prior to mounting the FS "
+read a b c
+mount /dev/$a/$b  /app/$c
+echo "following confirms the filesystem is mounted"
+mount | grep $c
